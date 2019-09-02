0e46e2e4dc224f4cdce7e43de2ac714e9dde109e
2309188136 <53836098+2309188136@users.noreply.github.com>
2019-09-01 23:13:31 -0500

    refactored recipe project with lombok support

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,8 +1,13 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
 //https://github.com/springframeworkguru/spring5-recipe-app/tree/one-to-one-jpa
+@Data
+@EqualsAndHashCode(exclude = "recipe") //exclude bidirection references to eliminate circular references
 @Entity
 public class Notes {
 
@@ -16,37 +21,4 @@ public class Notes {
     @Lob
     private String recipeNotes; //jpa-hibernate has limit of 250 characters,mark lob solve the issue
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Notes{");
-        sb.append("id=").append(id);
-        sb.append(", recipe=").append(recipe);
-        sb.append(", recipeNotes='").append(recipeNotes).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
