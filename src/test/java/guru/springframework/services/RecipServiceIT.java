41591b7aa9b226cd6febdce407b235370266059c
2309188136 <53836098+2309188136@users.noreply.github.com>
2019-09-07 13:38:46 -0500

    added form command objects and corresponding converters

---
+++ src/test/java/guru/springframework/services/RecipServiceIT.java
@@ -0,0 +1,72 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.converters.RecipeCommandToRecipe;
+import guru.springframework.converters.RecipeToRecipeCommand;
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipServiceIT {
+
+
+    public static final String NEW_DESCRIPTION = "New Description";
+
+    @Autowired
+    RecipeService recipeService;
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
+    @Autowired
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Transactional
+    @Test
+    public void testSaveOfDescription() throws Exception {
+
+        /**
+         * if object is the same jpaCrudRepository save method behave like update
+         * automatically does what you want. If the entity is new it will call persist on the entity manager, otherwise it will call merge
+         *
+         * The code looks like this:
+         *
+         * public <S extends T> S save(S entity) {
+         *
+         *     if (entityInformation.isNew(entity)) {
+         *         em.persist(entity);
+         *         return entity;
+         *     } else {
+         *         return em.merge(entity);
+         *     }
+         * }
+         */
+        //given
+        Iterable<Recipe> recipes = recipeRepository.findAll();
+        Recipe testRecipe = recipes.iterator().next();
+        RecipeCommand testRecipeCommand = recipeToRecipeCommand.convert(testRecipe);
+
+        //when
+        testRecipeCommand.setDescription(NEW_DESCRIPTION);
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(testRecipeCommand);
+
+        Iterable<Recipe> recipes2 = recipeRepository.findAll();
+        //then
+        assertEquals(NEW_DESCRIPTION, savedRecipeCommand.getDescription());
+        assertEquals(testRecipe.getId(), savedRecipeCommand.getId());
+        assertEquals(testRecipe.getCategories().size(), savedRecipeCommand.getCategories().size());
+        assertEquals(testRecipe.getIngredients().size(), savedRecipeCommand.getIngredients().size());
+    }
+}
